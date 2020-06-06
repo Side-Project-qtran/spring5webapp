c401fad87c0943b10d3e66061fd0abfcad5073f6
Quan Tran <Quan.Tran@docusign.com>
2020-06-05 20:12:02 -0700

    in progress

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5webappApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		SpringApplication.run(Spring5webappApplication.class, args);
 	}
 }
